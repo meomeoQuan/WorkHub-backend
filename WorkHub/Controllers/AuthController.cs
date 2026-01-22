91c6af0041c1e4210dc30bb0ab0ed7e0e5b4a3d0
meomeoQuan <QUANNADE180924@fpt.edu.vn>
2026-01-23 00:14:00 +0700

    Adding auth controller

---
+++ WorkHub/Controllers/AuthController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WorkHub.Business.Service.IService;
+using WorkHub.Models.DTOs;
+
+namespace WorkHub.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterRequest registerRequest)
+        { 
+            await _authService.RegisterAsync(registerRequest);
+            return Ok();
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequest loginRequest)
+        {
+            var token = await _authService.LoginAsync(loginRequest);
+            return Ok(new { Token = token });
+        }
+    }
+}
