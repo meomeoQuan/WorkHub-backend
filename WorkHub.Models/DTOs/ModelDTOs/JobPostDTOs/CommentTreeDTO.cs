aa8a912da7d04a10b0dc8c6d6cef65861dd84f15
meomeoQuan <QUANNADE180924@fpt.edu.vn>
2026-03-06 12:59:27 +0700

    Add delete , edit comment

--- WorkHub.Models/DTOs/ModelDTOs/JobPostDTOs/CommentTreeDTO.cs
+++ WorkHub.Models/DTOs/ModelDTOs/JobPostDTOs/CommentTreeDTO.cs
@@ -9,6 +9,7 @@ namespace WorkHub.Models.DTOs.ModelDTOs.JobPostDTOs
     public class CommentTreeDTO
     {
         public int Id { get; set; }
+        public int UserId { get; set; }
         public string UserName { get; set; }
         public string? Content { get; set; }
         public string? UserUrl { get; set; }
